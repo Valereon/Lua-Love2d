8f0dabd1ce6e6408cdd5348a86df0f338378f257
Valereon <tools418@gmail.com>
2023-03-26 17:29:37 -0400

    Made 1 procedual asteroid along with segmented destruction and destorying projectiles when they hit the asteroid

--- SpaceShipTesting/main.lua
+++ SpaceShipTesting/main.lua
@@ -1,9 +1,11 @@
 
 
 function love.load()
+    Camera = require("Libraries/camera")
+    Cam = Camera()
+    
 
-
-    Player = {x=200,y=200,speed=.5,velocityX=0,velocityY=0,drag=.95, rotX=0, rotSpeed=3,maxSpeed=5}
+    Player = {x=200,y=200,speed=.25,velocityX=0,velocityY=0,drag=.95, rotX=0, rotSpeed=3,}
     SpritePlayer = love.graphics.newImage("Sprites/space.png")
     TableProjectiles = {}
     Asteroid = {x=0,y=0,speed=.5,velocityX=0,velocityY=0,mass=10,radius=8}
@@ -17,42 +19,42 @@ end
 
 
 function love.draw()
-    -- love.graphics.setColor(0,0,0,255)
-    --player collison box debug
-    love.graphics.rectangle("line",Player.x - 18.5, Player.y - 18.5, 30,28)
-    -- love.graphics.setColor(255,255,255,255)
-    --player sprite
-    love.graphics.draw(SpritePlayer, Player.x, Player.y, math.rad(Player.rotX + 90), .5, .5, 32, 32, 0, 0)
+    Cam:attach()
+        -- love.graphics.setColor(0,0,0,255)
+        --player collison box debug
+        love.graphics.rectangle("line",Player.x - 8, Player.y - 8, 16,16)
+        -- love.graphics.setColor(255,255,255,255)
+        --player sprite
+        love.graphics.draw(SpritePlayer, Player.x, Player.y, math.rad(Player.rotX + 90), .25, .25, 32, 32, 0, 0)
 
-    
-    if #TableProjectiles > 0 then
-        for i = 1, #TableProjectiles, 1 do
-            --debug collison box for projectile ↓
-            love.graphics.rectangle("line", TableProjectiles[i].x - 15, TableProjectiles[i].y - 5, 10, 10)
+        
+        if #TableProjectiles > 0 then
+            for i = 1, #TableProjectiles, 1 do
+                --debug collison box for projectile ↓
+                -- love.graphics.rectangle("line", TableProjectiles[i].x - 15, TableProjectiles[i].y - 5, 10, 10)
 
-            love.graphics.circle("line",TableProjectiles[i].x,
-                                        TableProjectiles[i].y, 5)
+                love.graphics.circle("line",TableProjectiles[i].x,
+                                            TableProjectiles[i].y, 2.5)
+            end
         end
-    end
+    
+        for i = 1, #TableAsteroids, 1 do
+            love.graphics.rectangle("fill",TableAsteroids[i].x,TableAsteroids[i].y,
+                                        TableAsteroids[i].width,TableAsteroids[i].height)
+            
+        end
+    Cam:detach()
 
-    for i = 1, #TableAsteroids, 1 do
-        love.graphics.rectangle("fill",TableAsteroids[i].x,TableAsteroids[i].y,
-                                       TableAsteroids[i].width,TableAsteroids[i].height)
-        
-    end
-
-        
+            
 end
 
 function love.update(dt)
     UserInput()
     UpdateProjectiles(dt)
     Collisons()
-
-
-
-
-
+    ProjectileDeletion()
+    Cam:lookAt(Player.x,Player.y)
+    
 end
 
 
@@ -77,7 +79,6 @@ function UserInput()
     end
     if love.keyboard.isDown("space") then
         Shot = true
-        table.insert(TableProjectiles, {x=Player.x +(math.cos(math.rad(Player.rotX))),y=Player.y +(math.sin(math.rad(Player.rotX))),speed=.5,velocityX=5, velocityY=5, angle=Player.rotX})    
     else
         Shot = false
     end
@@ -92,6 +93,11 @@ end
 
 
 function UpdateProjectiles(dt)
+    if Shot then
+        table.insert(TableProjectiles, {x=Player.x +(math.cos(math.rad(Player.rotX))),
+                                        y=Player.y +(math.sin(math.rad(Player.rotX))),
+                                        speed=.5,velocityX=5, velocityY=5, angle=Player.rotX, dmg=5})            
+    end
     for i = 1, #TableProjectiles, 1 do
         TableProjectiles[i].x = TableProjectiles[i].x + (math.cos(math.rad(TableProjectiles[i].angle)) * TableProjectiles[i].velocityX)
         TableProjectiles[i].y = TableProjectiles[i].y + (math.sin(math.rad(TableProjectiles[i].angle)) * TableProjectiles[i].velocityY)
@@ -111,11 +117,6 @@ function UpdateProjectiles(dt)
 end
 
 
-
-
-
-
-
 function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
     return x1 < x2+w2 and
             x2 < x1+w1 and
@@ -125,18 +126,25 @@ end
 
 
 function Collisons()
+    Destroyed = false
     for ast = 1, #TableAsteroids, 1 do
+        if Destroyed then
+            break
+        end
+        local playerHit = CheckCollision(TableAsteroids[ast].x,TableAsteroids[ast].y,15,10, Player.x - 8, Player.y - 8, 16,16)
+        if playerHit then
+            print("PLAYER HIT", Player.x, " ", Player.y)
+            
+        end
         for proj = 1, #TableProjectiles, 1 do
-            local hit = CheckCollision(TableAsteroids[ast].x,TableAsteroids[ast].y,TableAsteroids[ast].width,TableAsteroids[ast].height,TableProjectiles[proj].x,TableProjectiles[proj].y,10,10)
-            local playerHit = CheckCollision(TableAsteroids[ast].x,TableAsteroids[ast].y,15,10, Player.x - 18.5, Player.y - 18.5, 30,28) 
+            local hit = CheckCollision(TableAsteroids[ast].x,TableAsteroids[ast].y,TableAsteroids[ast].width,TableAsteroids[ast].height,TableProjectiles[proj].x,TableProjectiles[proj].y,5,5)
             if hit then
                 print("HIT", TableAsteroids[ast].x, " ", TableAsteroids[ast].y)
-            end
-            if playerHit then
-                print("PLAYER HIT", Player.x, " ", Player.y)
-                
-            end
-            
+                AsteroidDamaged(ast,proj)
+                table.remove(TableProjectiles, proj)
+                Destroyed = true
+                break
+            end            
         end
     end
     
@@ -147,9 +155,62 @@ end
 
 
 function SpawnAsteroids()
-    for i = 1, 10, 1 do
-        table.insert(TableAsteroids, {x=love.math.random(1,love.graphics.getWidth()),
-                                      y=love.math.random(1,love.graphics.getHeight()),
-                                      velocityX=0, velocityY=0, mass=10,width=15,height=10})
+    -- for i = 1, 10, 1 do
+    --     table.insert(TableAsteroids, {x=love.math.random(1,love.graphics.getWidth()),
+    --                                   y=love.math.random(1,love.graphics.getHeight()),
+    --                                   velocityX=0, velocityY=0, mass=10,width=15,height=10})
+    -- end
+    -- format is x=?,y=?,width=?,height=?
+
+
+
+
+    --Beggning Asteroid
+    table.insert(TableAsteroids, {x=100,y=100,width=10,height=10,health=love.math.random(5,20)})
+    --Connecting Asteroids
+    for j = 1, 250, 1 do
+        local dir = love.math.random(1,4)
+        if dir == 1 then
+            table.insert(TableAsteroids, {x=TableAsteroids[#TableAsteroids].x + TableAsteroids[#TableAsteroids].width, y=TableAsteroids[#TableAsteroids].y, width=love.math.random(10,20), height=love.math.random(10,20),health=love.math.random(5,20)})
+        elseif dir == 2 then
+            table.insert(TableAsteroids, {x=TableAsteroids[#TableAsteroids].x - TableAsteroids[#TableAsteroids].width, y=TableAsteroids[#TableAsteroids].y, width=love.math.random(10,20), height=love.math.random(10,20),health=love.math.random(5,20)})
+        elseif dir == 3 then
+            table.insert(TableAsteroids, {x=TableAsteroids[#TableAsteroids].x, y=TableAsteroids[#TableAsteroids].y + TableAsteroids[#TableAsteroids].height, width=love.math.random(10,20), height=love.math.random(10,20),health=love.math.random(5,20)})
+        elseif dir == 4 then 
+            table.insert(TableAsteroids, {x=TableAsteroids[#TableAsteroids].x, y=TableAsteroids[#TableAsteroids].y - TableAsteroids[#TableAsteroids].height, width=love.math.random(10,20), height=love.math.random(10,20),health=love.math.random(5,20)})
+        end
+        
+    end    
+    
+    
+
+
+
+end
+
+
+function ProjectileDeletion()
+    if #TableProjectiles > 0 then
+        if TableProjectiles[#TableProjectiles].x > 50 + love.graphics.getWidth() then   
+            table.remove(TableProjectiles, #TableProjectiles)
+        else if TableProjectiles[#TableProjectiles].y > 50 + love.graphics.getHeight() then
+            table.remove(TableProjectiles, #TableProjectiles)
+        end
     end
+    
+        print(#TableProjectiles)
+    end
+end
+
+
+function AsteroidDamaged(Asteroid, projectile)
+    if TableAsteroids[Asteroid].health == nil then
+        return
+    end
+    TableAsteroids[Asteroid].health = TableAsteroids[Asteroid].health - TableProjectiles[projectile].dmg
+    if TableAsteroids[Asteroid].health <= 0 then
+        table.remove(TableAsteroids, Asteroid)
+    end
+    
+    
 end
